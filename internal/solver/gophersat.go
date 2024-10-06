5b1100ed5b437adeab69eb202848c61d4bdf5f88
Antoine Belvire <antoine.belvire@opensuse.org>
2024-10-06 20:45:15 +0200

    feat: Iterate over models returned by gophersat
    Solver is still broken but the iteration
    logic should be good now.

--- internal/solver/gophersat.go
+++ internal/solver/gophersat.go
@@ -21,7 +21,6 @@ func NewGophersatSolver() spi.ConfigurableSolver {
 	return &solverConfigurer
 }
 
-// AddClause adds the given literals as an *at-least-one* clause, i.e. a disjunction (= or).
 func (s *gophersatSolver) AddClause(spiLiterals []spi.Literal) {
 	literals := gophersatLitsFrom(spiLiterals...)
 	clause := sat.NewClause(literals)
@@ -39,13 +38,17 @@ func gophersatLitsFrom(vals ...spi.Literal) []sat.Lit {
 	return res
 }
 
-// Solutions returns an iterator on the solutions.
 func (s *gophersatSolver) Solutions() iter.Seq[spi.Model] {
 	return func(yield func(spi.Model) bool) {
-		// TODO actually enumerate all solutions
-		status := s.satSolver.Solve()
-		if status == sat.Sat {
-			yield(s.satSolver.Model())
+		models := make(chan []bool)
+		stop := make(chan struct{}, 1) // FIXME Enumerate does not read from stop channel?
+		defer close(stop)
+		go s.satSolver.Enumerate(models, stop)
+		for model := range models {
+			if keepGoing := yield(model); !keepGoing {
+				stop <- struct{}{}
+				break
+			}
 		}
 	}
 }
